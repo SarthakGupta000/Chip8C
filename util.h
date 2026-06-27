6a22cf19db2d895ccff687a75a68fd73ef9f706a
Sarthak Gupta <sarthaksrws@icloud.com>
2026-06-27 14:21:37 +0530

    Adding some more helpful variables and functions

--- util.h
+++ util.h
@@ -1,5 +1,15 @@
 #include <stdint.h>
 #include <stdlib.h>
+#define STRING_VAR(variable) #variable // converts enum
+
+enum Instructions { // o signifies empty space to keep it in 3 bytes
+    MOV,
+    ADD,
+    SUB,
+    JMP,
+};
+
+int instructionMap[4][3];
 
 struct CPU {
     uint8_t ram[4096];
@@ -11,12 +21,14 @@ struct CPU {
     // uint8_t carryFlag;
 };
 
-uint8_t fetch(char *reg); // like R0 R1 R2 R3
+void mapInstructions();
+
+uint8_t fetch(char *reg); // like R0 R1 R2 R3 and also all 1 byte integers
 
 char **splitProgram(char *program);
 
 char **splitLine(char *line);
 
-uint8_t execute(char **line); // split line
+uint8_t execute(char **line); // split line as input
 
 void run(char *program);
